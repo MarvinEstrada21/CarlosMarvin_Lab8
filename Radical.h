75e926254450bef086d56dd93e6b2364a4901c9f
Carlos <alfreoldoriverac25@gmail.com>
2017-06-09 13:38:55 -0600

    clases

---
+++ Radical.h
@@ -0,0 +1,19 @@
+#include "Real.h"
+#ifndef RADICAL_H
+#define RADICAL_H
+
+class Radical: public Real{
+	private:
+		double coeficinete;
+		double indice;
+		double radicando;
+	public:
+		double getCoeficiente();
+		void setCoeficiente();
+
+		double getIndice();
+		void setIndice();
+
+		double getRadicando();
+		void setRadicando();
+}
