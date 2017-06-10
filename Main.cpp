520d8ae7e6128d682e37e002d7e0374368c8e994
Carlos <alfreoldoriverac25@gmail.com>
2017-06-09 18:15:29 -0600

    menus

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <typeinfo>
+
 
 using namespace std;
 int menu();
@@ -11,12 +13,14 @@ int menuAgregar();
 int menuOperaciones();
 int main() {
 	vector <Real*> banco;
-	bool salir=false;
-	while(!salir){
+	vector <Radical*> listaradical;
+	vector <Racional*> listaracional;
+	bool salirTodo=false;
+	while(!salirTodo){
 		switch(menu()){
 			case 1:{
-				bool salirOperaciones =false;
-				while(!salirOperaciones){
+				bool salirAgregar =false;
+				while(!salirAgregar){
 					switch(menuAgregar()){
 						case 1:{
 							int temporal;
@@ -41,21 +45,7 @@ int main() {
 							break;
 						}
 						case 3:{
-							/*int numero;
-							cout<<"Ingrese un numero: "<<endl;
-							cin>>numero;
-							banco.push_back(new Real&(numero));
-							break;*/
-						}
-						case 4:{
-							/*double numeroDecimal;
-							cout<<"Ingrese un numero decimal: ";
-							cin>>numeroDecimal;
-							banco.push_back(numeroDecimal);
-							break;*/
-						}
-						case 5:{
-							salirOperaciones=true;
+							salirAgregar=true;
 							break;
 						}
 					}
@@ -64,10 +54,48 @@ int main() {
 			}
 
 			case 2:{
+				bool salirOperaciones=false;
+				while(!salirOperaciones){
+					switch(menuOperaciones()){
+						case 1:{
+							//sumar
+							int opcionsuma;
+							cout<<"1) Radical "<<endl
+								<<"2) Racional "<<endl;
+							cout<<"Seleccion una clase "<<endl;
+							cin>>opcionsuma;
+							if(opcionsuma==1){
+								Radical* temp=dynamic_cast<Radical*>(banco[0]);	
+							}
+							if(opcionsuma==2){
+								Racional* temp=dynamic_cast<Racional*>(banco[0]);
+							}
+
+							break;							
+						}
+						case 2:{
+							//resta
+							break;
+						}
+						case 3:{
+							//multiplicar
+							break;
+						}
+						case 4:{
+							//division
+							break;
+						}
+						case 5:{
+							salirOperaciones=true;
+							break;
+						}
+					}
+				}
+
 				break;
 			}
 			case 3:{
-				salir=true;
+				salirTodo=true;
 				break;				
 			}
 		}
@@ -103,13 +131,11 @@ int menuAgregar(){
 		cout<< "-----MENU AGREGAR -----" << endl
 			<< "1.- Agregar Radical" << endl
 			<< "2.- Agregar Racional " << endl
-			<< "3.- Agregar Enteros "<<endl
-			<< "4.- Agregar Decimales "<<endl
-			<< "5.- Salir" << endl;
+			<< "3.- Salir" << endl;
 		
 		cout<< "Ingrese una opcion: ";
 		cin>>opcion;
-		if (opcion > 0 && opcion <= 5)
+		if (opcion > 0 && opcion <= 3)
 			valido = true;
 		else {
 			cout << "Opcion no valida, intente de nuevo..." << endl;
@@ -124,15 +150,48 @@ int menuOperaciones(){
 	bool valido = false;
 	do {
 		cout<< "-----MENU DE OPERACIONES -----" << endl
-			<< "1.- Agregar Radical" << endl
-			<< "2.- Agregar Racional " << endl
-			<< "3.- Agregar Enteros "<<endl
-			<< "4.- Agregar Decimales "<<endl
+			<< "1.- Sumar" << endl
+			<< "2.- Restar " << endl
+			<< "3.- Multiplicacion "<<endl
+			<< "4.- Division "<<endl
 			<< "5.- Salir" << endl;
 		
 		cout<< "Ingrese una opcion: ";
 		cin>>opcion;
 		if (opcion > 0 && opcion <= 5)
+			valido = true;
+		else {
+			cout << "Opcion no valida, intente de nuevo..." << endl;
+		}
+
+	} while (!valido);
+	return opcion;
+}
+
+int menuSuma(){
+	int opcion;
+	bool valido = false;
+	do {
+		cout<< "-----MENU DE SUMA -----" << endl
+			<< "1.- Radical con Radical" << endl
+			<< "2.- Radical con Racional " << endl
+			<< "3.- Radical con Enteros "<<endl
+			<< "4.- Radical con Decimales "<<endl
+			<< "5.- Racional con Racional "<<endl
+			<< "6.- Racional con Radical "<<endl
+			<< "7.- Racional con Enteros "<<endl
+			<< "8.- Racional con Decimales "<<endl
+			<< "9.- Enteros con Decimales "<<endl
+			<< "10.- Enteros con Racionales "<<endl
+			<< "11.- Enteros con Radical "<<endl
+			<< "12.- Decimales con Racional "<<endl
+			<< "13.- Decimales con Radical "<<endl
+			<< "14.- Decimales con Enteros "<<endl
+			<< "15.- Salir" << endl;
+		
+		cout<< "Ingrese una opcion: ";
+		cin>>opcion;
+		if (opcion > 0 && opcion <= 15)
 			valido = true;
 		else {
 			cout << "Opcion no valida, intente de nuevo..." << endl;
